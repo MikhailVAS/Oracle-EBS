fb785426d65f876ed57b2a070f149e0221e12352
MiKhail Vasiljev <mikhailvas@gmail.com>
2018-08-15 17:03:58 +0300

    Add Info

--- Order Import Error .sql
+++ Order Import Error .sql
@@ -3,6 +3,15 @@
 "Concurrent cannot be  Cancelled" for the parent request, Parent request
   status is 'Paused',  the child requests show as running
 
+############################################################
+INFO
+concurrent - XXTG: Processing internal requsition to Pick Wave Order
+Start childs  concurrents :
+1) Create Internal Orders
+2) Order Import
+3) Order Import Child Req1 (Order Import)
+4) Pick Selection List Generation – SRS (3 шт)
+############################################################
 
 Line	Oracle E-Business Suite	
 Area	Order Management
@@ -19,7 +28,7 @@ DELETE
  WHERE     order_source_id = 10
        AND orig_sys_document_ref IN (SELECT requisition_header_id
                                        FROM APPS.po_requisition_headers_all
-                                      WHERE segment1 IN ('78278'));
+                                      WHERE segment1 IN ('78277'));
 
 /* Delete the duplicate records in lines iface all. */
 DELETE 
@@ -27,9 +36,9 @@ DELETE
  WHERE     order_source_id = 10
        AND orig_sys_document_ref IN (SELECT requisition_header_id
                                        FROM APPS.po_requisition_headers_all
-                                      WHERE segment1 IN ('78278')); 
+                                      WHERE segment1 IN ('78277')); 
 									  
 /* update the error flag  to NULL so that all the headers  will be picked by Order Import Concurrent request for Processing.*/
 UPDATE PO.PO_REQUISITION_HEADERS_ALL PRH
    SET TRANSFERRED_TO_OE_FLAG = 'N'
- WHERE PRH.SEGMENT1 IN ('78278')
+ WHERE PRH.SEGMENT1 IN ('78277')
