2d23f78cf4750d5982ab1f6375a3ee139de03d6d
MiVasL <mikhailvas@gmail.com>
2022-07-29 11:02:53 +0300

    Clear XXTG_

--- XXTG_Refiral Sim.sql
+++
@@ -1,3 +0,0 @@
-SELECT *
-      FROM tm_cim.referral_sim_card@sm_deepdb.best.local
-
--- XXTG_Report.sql
+++
@@ -1,5 +0,0 @@
-/* Formatted on 30.07.2020 15:23:43 (QP5 v5.326) Service Desk 406689 Mihail.Vasiljev */
-/* Не отображается в отчёте BeST OnHand Quantity */ 
-UPDATE inv.mtl_system_items_b
-   SET ATTRIBUTE21 = NULL
- WHERE SEGMENT1 IN ('1013070537','1013070536')
